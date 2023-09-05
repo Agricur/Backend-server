8c99f5a54a3e98f8443197164cc42f06f82573f3
ChavinduRansara <119533194+ChavinduRansara@users.noreply.github.com>
2023-09-06 04:55:37 +0530

    install bcrypt and hash the password in buyer and seller controller

--- controller/buyer.js
+++ controller/buyer.js
@@ -1,38 +1,49 @@
 const pool = require("../db/db");
 const User = require("../model/buyer");
-
+const bcrypt = require('bcrypt');
  
 const createUser = async (req, res) => {
   const ip_address = req.ip;
   const { first_name, last_name, contact_no, email, password} = req.body;
-  pool.query(User.checkMail, [email], (error, results) => {
-    if (results.rows.length) {
-        res.status(400).json({ message: 'Email already exists' });
+
+  const plaintextPassword = password; 
+  const saltRounds = 10;
+  bcrypt.hash(plaintextPassword, saltRounds, (err, hash) => {
+    if (err) {
+      throw err;
     } else {
-      pool.query(User.createUser, [ip_address, first_name, last_name, contact_no, email, password],(error, results) => {
-          if (error) {
-            throw error;
-          } else {
-            const {email,number,street,city,district} = req.body;
-                pool.query(User.getUserID,[email],(error,id)=>{
-                    if(error){
-                        throw error;
-                    }else{
-                        user_id = parseInt(id.rows[0].user_id); 
-                        pool.query(User.insertUserAdress,[user_id,number,street,city,district],(error,results)=>{
-                          if(error){
-                              throw error;
-                          }else{
-                              res.status(201).json("user craeted successfully");
-                          }
-                      })        
-                  }
-                })
-          }
+      const hashedPassword = hash;
+      pool.query(User.checkMail, [email], (error, results) => {
+        if (results.rows.length) {
+            res.status(400).json({ message: 'Email already exists' });
+        } else {
+          pool.query(User.createUser, [ip_address, first_name, last_name, contact_no, email, hashedPassword],(error, results) => {
+              if (error) {
+                throw error;
+              } else {
+                const {email,number,street,city,district} = req.body;
+                    pool.query(User.getUserID,[email],(error,id)=>{
+                        if(error){
+                            throw error;
+                        }else{
+                            user_id = parseInt(id.rows[0].user_id); 
+                            pool.query(User.insertUserAdress,[user_id,number,street,city,district],(error,results)=>{
+                              if(error){
+                                  throw error;
+                              }else{
+                                  res.status(201).json("user craeted successfully");
+                              }
+                          })        
+                      }
+                    })
+              }
+            }
+          );
         }
-      );
+      });
     }
   });
+  
 };
 
 

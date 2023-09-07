8742fc973ca949cb5a7c0413a3cd817f2f037804
ChavinduRansara <119533194+ChavinduRansara@users.noreply.github.com>
2023-09-07 20:54:10 +0530

    create jwt tokens and cookies

--- controller/user.js
+++ controller/user.js
@@ -1,17 +1,21 @@
 const pool = require("../db/db");
 const User = require("../model/user");
+const jwt = require("jsonwebtoken");
 
 const loginUser = async (req, res) => {
     const {email,password} = req.body;
     const emailExists = await User.checkMail(email);
-    console.log(emailExists)
     if(!emailExists){
         res.status(400).json({ message: "Account doesn't exists!" });
     }else{
         const passwordExists = await User.checkPassword(password,email);
-        // console.log(passwordExists)
         if(passwordExists){
-            res.status(201).json({ message: "Successfully logged in!" });
+            const u_id = await User.getUserID(email); 
+            const token = jwt.sign({ u_id }, process.env.JWT_SECRET,{
+                expiresIn: process.env.JWT_EXPIRES_TIME,
+            }); 
+            
+            res.status(201).json({ message: "Successfully logged in!",token: token });
         }else{
             res.status(400).json({ message: "Username or Password is incorrect!" });
         } 

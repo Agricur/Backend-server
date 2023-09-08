73b27ee5f857ce34d811f7246e1783643c4c0cb6
ChavinduRansara <119533194+ChavinduRansara@users.noreply.github.com>
2023-09-08 20:51:20 +0530

    implement function to get user data and update the user profile in header

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,7 @@
 const pool = require("../db/db");
 const User = require("../model/user");
 const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
 
 const loginUser = async (req, res) => {
     const {email,password} = req.body;
@@ -10,18 +11,56 @@ const loginUser = async (req, res) => {
     }else{
         const passwordExists = await User.checkPassword(password,email);
         if(passwordExists){
-            const u_id = await User.getUserID(email); 
-            const token = jwt.sign({ u_id }, process.env.JWT_SECRET,{
+            const {user_id,is_seller} = await User.getUser(email); 
+            const token = jwt.sign({ user_id,is_seller }, process.env.JWT_SECRET,{
                 expiresIn: process.env.JWT_EXPIRES_TIME,
             }); 
             
-            res.status(201).json({ message: "Successfully logged in!",token: token });
+            res.status(201).json({ message: "Successfully logged in!",token: token, user_id: user_id, is_seller: is_seller });
         }else{
             res.status(400).json({ message: "Username or Password is incorrect!" });
         } 
     }
 };
 
+const getData = async (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (err) {
+          return res.status(403).json({ error: 'Invalid token' });
+        }
+    
+        // At this point, 'decoded' contains the user information (e.g., user ID)
+        const userId = decoded.user_id;
+        const isSeller = decoded.is_seller;
+        // Fetch user data from database based on 'userId'
+        const user = await User.getUserData(userId);
+        if (!user) {
+          return res.status(404).json({ error: 'User not found' });
+        }
+        first_name = user.first_name;
+        // Send user data as a response
+        res.json({
+          first_name: first_name,
+        });
+      });
+}
+
+
+// const isLogged = async (req, res, next) => {
+//     if(req.cookie.jwt){
+//         try{
+//             const decoded = await promisify(jwt.verify)(req.cookie.jwt, process.env.JWT_SECRET) ;
+//             const user = await User.getUser(decoded.u_id);
+//             req.user = user;
+//             next();
+//         }catch(err){
+//             res.status(401).json({ message: "Invalid token!" });
+//         }
+//     }
+// };
+
 module.exports = {
     loginUser,
+    getData,
 }

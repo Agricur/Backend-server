9c45d8d173bd782aaadf6e25bca68d6ed8afe0d6
Tishani Rathnasiri <tishani.20@cse.mrt.ac.lk>
2023-10-26 10:50:43 +0530

    Delete db.js

--- db/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg'); 
-const pool = new Pool({
-    connectionString : "postgres://root:02REtWgf6YJbi3yIRaP2FCdPGlCBbQvO@dpg-ckrrns05vl2c738hpsv0-a.singapore-postgres.render.com/agricur_database",
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-
-module.exports = pool;

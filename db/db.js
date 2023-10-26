f4bdb332b7df9f59a2ce10bd0b3278f3d89aa8c2
Tishani Rathnasiri <tishani.20@cse.mrt.ac.lk>
2023-10-26 10:06:38 +0530

    To  resolve conflicts.
    To resolve conflicts.

--- db/db.js
+++ db/db.js
@@ -1,11 +1,9 @@
-const Pool = require('pg').Pool
+const { Pool } = require('pg'); 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'Agricur',
-    host: 'localhost',
-    port: 5432,
-    database: 'Agricur'
-})
+    connectionString : "postgres://root:02REtWgf6YJbi3yIRaP2FCdPGlCBbQvO@dpg-ckrrns05vl2c738hpsv0-a.singapore-postgres.render.com/agricur_database",
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 module.exports = pool;
-
